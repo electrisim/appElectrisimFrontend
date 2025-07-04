647f93e1125b5bc5a98beed4221e9737dc94b3ce
kglikk <kglikk@gmail.com>
2025-07-04 18:22:16 +0200

    dropbox and onedrive error corrected

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -1,7 +1,13 @@
-
 window.EXPORT_URL = 'REPLACE_WITH_YOUR_IMAGE_SERVER';
 window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
-window.DRAWIO_BASE_URL = null; 
+// Set the base URL explicitly for your domain
+window.DRAWIO_BASE_URL = window.location.protocol + '//' + window.location.hostname;
+// Prevent overriding by making it non-configurable
+Object.defineProperty(window, 'DRAWIO_BASE_URL', {
+    value: window.location.protocol + '//' + window.location.hostname,
+    writable: false,
+    configurable: false
+});
 window.DRAWIO_VIEWER_URL = null;
 window.DRAW_MATH_URL = 'math';
 window.DRAWIO_CONFIG = null; 
